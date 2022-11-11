98f0c62a6296bc33de9ae7a7b502a3376a91008c
Hu Xiaoqun <xqhu2008@hotmail.com>
2022-11-11 21:38:19 +0800

    add sudoku solve algorithm

--- Trilobites/src/sudoku/sudoku.py
+++ Trilobites/src/sudoku/sudoku.py
@@ -30,6 +30,43 @@ class Sudoku:
     def __setitem__(self, key, value):
         self._boards[key] = value
 
+    def findEmpty(self):
+        for i in Sudoku.rows:
+            for j in Sudoku.cols:
+                if self[i, j] == 0:
+                    return i, j
+        return Sudoku.rows, Sudoku.cols
+
+    def isValid(self, row, col, num):
+        if num in self[row, :]:
+            return False
+
+        if num in self[:, col]:
+            return False
+
+        rowBlockStart, rowBlockEnd = row // 3 * 3, row // 3 * 3 + 3
+        colBlockStart, colBlockEnd = col // 3 * 3, col // 3 * 3 + 3
+        if num in self[rowBlockStart : rowBlockEnd, colBlockStart : colBlockEnd]:
+            return False
+
+        return True
+
+    def solve(self):
+        row, col = self.findEmpty()
+        if row >= Sudoku.rows or col >= Sudoku.cols:
+            return True
+
+        for num in range(1, Sudoku.rows):
+            if self.isValid(row, col, num):
+                self[row, col] = num
+
+                if self.solve():
+                    return True
+
+                self[row, col] = 0
+
+        return False
+
     def loadFomString(self, sudo):
         s = sudo.split(', ')
         self._level = self._parseSudokuLevel(s[0])
@@ -58,15 +95,16 @@ class Sudoku:
     @staticmethod
     def buildSudoku(level=SudokuLevel.P):
         sudo = Sudoku()
-        sudo.loadFomString("P, 0, 4, 0, 0, 0, 2, 0, 1, 9, "
-                              "0, 0, 0, 3, 5, 1, 0, 8, 6, "
-                              "3, 1, 0, 0, 9, 4, 7, 0, 0, "
-                              "0, 9, 4, 0, 0, 0, 0, 0, 7, "
-                              "0, 0, 0, 0, 0, 0, 0, 0, 0, "
-                              "2, 0, 0, 0, 0, 0, 8, 9, 0, "
-                              "0, 0, 9, 5, 2, 0, 0, 4, 1, "
-                              "4, 2, 0, 1, 6, 9, 0, 0, 0, "
-                              "1, 6, 0, 8, 0, 0, 0, 7, 0")
+        sudo.loadFomString("P, "
+                           "0, 4, 0, 0, 0, 2, 0, 1, 9, "
+                           "0, 0, 0, 3, 5, 1, 0, 8, 6, "
+                           "3, 1, 0, 0, 9, 4, 7, 0, 0, "
+                           "0, 9, 4, 0, 0, 0, 0, 0, 7, "
+                           "0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                           "2, 0, 0, 0, 0, 0, 8, 9, 0, "
+                           "0, 0, 9, 5, 2, 0, 0, 4, 1, "
+                           "4, 2, 0, 1, 6, 9, 0, 0, 0, "
+                           "1, 6, 0, 8, 0, 0, 0, 7, 0")
 
         return sudo
 
@@ -75,5 +113,14 @@ if __name__ == "__main__":
     sudo = Sudoku()
     print(sudo)
 
-    sudo.loadFomString("P, 0, 4, 0, 0, 0, 2, 0, 1, 9, 0, 0, 0, 3, 5, 1, 0, 8, 6, 3, 1, 0, 0, 9, 4, 7, 0, 0, 0, 9, 4, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 8, 9, 0, 0, 0, 9, 5, 2, 0, 0, 4, 1, 4, 2, 0, 1, 6, 9, 0, 0, 0, 1, 6, 0, 8, 0, 0, 0, 7, 0")
+    sudo.loadFomString("P, "
+                       "0, 4, 0, 0, 0, 2, 0, 1, 9, "
+                       "0, 0, 0, 3, 5, 1, 0, 8, 6, "
+                       "3, 1, 0, 0, 9, 4, 7, 0, 0, "
+                       "0, 9, 4, 0, 0, 0, 0, 0, 7, "
+                       "0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                       "2, 0, 0, 0, 0, 0, 8, 9, 0, "
+                       "0, 0, 9, 5, 2, 0, 0, 4, 1, "
+                       "4, 2, 0, 1, 6, 9, 0, 0, 0, "
+                       "1, 6, 0, 8, 0, 0, 0, 7, 0")
     print(sudo)
